32cf81c0b8a8c41b8c5bb25cfb02fa8d4ac619ce
Clover <71147866+EmpressClover@users.noreply.github.com>
2023-10-08 16:07:24 -0700

    Finalized News

---
+++ dragonballarena.js
@@ -0,0 +1,30 @@
+var images = [
+    'images/banners/15.webp',
+    'images/banners/2.webp',
+    'images/banners/3.webp',
+    'images/banners/4.webp',
+    'images/banners/5.webp',
+    'images/banners/6.webp',
+    'images/banners/7.webp',
+    'images/banners/8.webp',
+    'images/banners/9.webp',
+    'images/banners/10.webp',
+    'images/banners/11.webp',
+    'images/banners/12.webp',
+    'images/banners/13.webp',
+    'images/banners/14.webp',
+    'images/banners/1.webp'
+    ]
+    
+    var which = Math.floor(Math.random() * images.length);
+    var img = document.getElementById('banner');
+    img.src = images[which];
+    img.title = images[which];
+    
+    
+    $(document).ready(function() {
+        $('.newsimage').hide();
+        $('.newsimage:lt(5)').show();
+    });
+    
+    
