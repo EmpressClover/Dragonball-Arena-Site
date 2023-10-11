5a57bbaeed9b533e8d9650b24c134c944b51ee22
Clover <71147866+EmpressClover@users.noreply.github.com>
2023-10-10 20:35:04 -0700

    FINALIZED LOGIN

--- dragonballarena.js
+++ dragonballarena.js
@@ -22,9 +22,3 @@ var images = [
     img.title = images[which];
     
     
-    $(document).ready(function() {
-        $('.newsimage').hide();
-        $('.newsimage:lt(5)').show();
-    });
-    
-    
